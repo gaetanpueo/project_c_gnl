2d740e8192a6cd1d8e7d3b35e05292e233f22c3d
Gaetan PUEO--GOURNAY <gpueo--g@e1r13p11.42.fr>
2015-03-08 13:28:21 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# project_c_gnl
+# README #
+
+Projet : get_next_line
+
+### Sources ###
+
+* get_next_line.c
+* get_next_line.h
+
+Must contain [libft](https://github.com/gaetanpueo/project_c_libft) in the folder ../libft
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpueo--g <gpueo--g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/25 11:49:24 by gpueo--g          #+#    #+#             */
+/*   Updated: 2014/11/25 11:49:25 by gpueo--g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/includes/libft.h"
+
+# define BUFF_SIZE 8
+
+int		get_next_line(int const fd, char **line);
+
+#endif
